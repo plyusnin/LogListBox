40437ff016abee4c04ea671b6893864047bd4102
Plyusnin Evgeniy <eninbox@gmail.com>
2020-06-12 13:44:15 +0500

    Filtration in demo application added

--- LogList.Demo/ViewModels/MainViewModel.cs
+++ LogList.Demo/ViewModels/MainViewModel.cs
@@ -11,13 +11,21 @@ namespace LogList.Demo.ViewModels
 {
     public class MainViewModel : ReactiveObject
     {
+        private string _filter;
         private int _id;
 
         public MainViewModel()
         {
             var itemsSource = new SourceList<MyLogItem>();
 
+            var filter = this.WhenAnyValue(x => x.Filter)
+                             .Select(ft => ft?.Trim())
+                             .Select(rq => string.IsNullOrWhiteSpace(rq)
+                                         ? (Func<MyLogItem, bool>) (i => true)
+                                         : (Func<MyLogItem, bool>) (i => i.ToString().Contains(rq)));
+
             itemsSource.Connect()
+                       .Filter(filter)
                        .Cast(x => (ILogItem) x)
                        .Bind(out var items)
                        .Subscribe();
@@ -28,13 +36,21 @@ namespace LogList.Demo.ViewModels
                                      .ObserveOn(TaskPoolScheduler.Default)
                                      .Do(_ => itemsSource.Add(
                                              new MyLogItem(Interlocked.Increment(ref _id), DateTime.Now)));
-            
-            var demoData = Enumerable.Range(0, 100).Select(i => new MyLogItem(Interlocked.Increment(ref _id), DateTime.Now.AddMinutes(i)));
-            
+
+            var demoData = Enumerable.Range(0, 100)
+                                     .Select(i => new MyLogItem(Interlocked.Increment(ref _id),
+                                                                DateTime.Now.AddMinutes(i)));
+
             inserter.Subscribe();
             itemsSource.AddRange(demoData);
         }
 
         public ListViewModel Items { get; set; }
+
+        public string Filter
+        {
+            get => _filter;
+            set => this.RaiseAndSetIfChanged(ref _filter, value);
+        }
     }
 }
