59caac8d9627d9fb1f5e1920436c9fc9e2f9cc7e
Plyusnin Evgeniy <eninbox@gmail.com>
2020-06-22 13:00:42 +0500

    DispatcherDistinctExtensions

---
+++ LogList.Control/DispatcherDistinctExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace LogList.Control
+{
+    public static class DispatcherDistinctExtensions
+    {
+        public static IObservable<T> DistinctByDispatcher<T>(this IObservable<T> Source, DispatcherPriority Priority)
+            where T : class
+        {
+            T val = default;
+            return Source.Do(v => Interlocked.Exchange(ref val, v))
+                         .ObserveOnDispatcher()
+                         .Select(_ => val)
+                         .DistinctUntilChanged();
+        }
+    }
+}
